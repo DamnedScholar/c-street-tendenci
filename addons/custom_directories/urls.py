9e0ae23d7ad2cf9a71d8133d85709f32dded0ec9
DamnedScholar <stickfigureonfire@gmail.com>
2020-07-29 11:19:00 -0500

    Old stuff getting in the way.

--- addons/custom_directories/urls.py
+++ addons/custom_directories/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import url
 from tendenci.apps.site_settings.utils import get_setting
 from . import views
 
-urlpath = get_setting('module', 'directories', 'url')
-if not urlpath:
-    urlpath = "directories"
+urlpath = "directory"
 
 urlpatterns = [
     # Custom category pages
     # TODO: Clean this up and make the category list pull from a canonical source instead of being hardcoded.
     # url(r'^%s/(?P<cat>(%s))/$' % (urlpath, views.CustomCats().cats_regex()), views.category, name="directory.category"),
-    url(r'^%s/(?P<cat>(food-and-drink|shopping|lifestyle|personal-services|venues-and-events|rentals|professionals))/$' % (urlpath), views.category, name="directory.category"),
-    url(r'^%s/(?P<cat>(food-and-drink|shopping|lifestyle|personal-services|venues-and-events|rentals|professionals))/print/$' % (urlpath), views.print, name="directory.print"),
+    url(r'^%s/(?P<cat>(food-and-drink|shopping|lifestyle|services|venues-and-events|rental))/$' % (urlpath), views.category, name="directory.category"),
+    url(r'^%s/(?P<cat>(food-and-drink|shopping|lifestyle|services|venues-and-events|rental))/print/$' % (urlpath), views.print, name="directory.print"),
 ]
