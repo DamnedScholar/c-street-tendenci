ed0b3a4cb6e56396f8f91862b1db9f9d71c99b95
DamnedScholar <stickfigureonfire@gmail.com>
2020-07-29 11:01:54 -0500

    Sometimes the error is really stupid and the quickest solution is to do something dirty.

--- addons/custom_directories/templatetags/custom_dir_tags.py
+++ addons/custom_directories/templatetags/custom_dir_tags.py
@@ -46,7 +46,7 @@ def imgproxy(url):
 
     current_site = Site.objects.get_current()
     # TODO: This is a dirty hack to get the imgproxy host and port into the URL. Need to make it less shitty by pointing to a config or something.
-    prefix = "https://" + current_site.domain + "/imgproxy"
+    prefix = "https://eclectic-co.com/imgproxy"
 
     options = {
         'resize': 'fill',
